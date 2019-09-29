80d8ef1561a4502fbad3d327347cc2bc977d075a
Henry <henry@typheus.me>
2019-09-29 11:55:23 -0600

    Update data.py

--- cogs/utils/data.py
+++ cogs/utils/data.py
@@ -76,7 +76,7 @@ class ResourceManager:
         if resource in self.locks:
             lock = self.locks[resource]
             lock.release()
-            if lock.locked():
+            if not lock.locked():
                 del self.locks[resource]
         else:
             raise RuntimeError("This lock is not being held!")
@@ -85,9 +85,8 @@ class ResourceManager:
         return ContextManagerLockWrapper(self, resource)
 
 
-
 class Character(tuple):
-    'Character(name, owner, description, level, team, meta)'
+    """Character(name, owner, description, level, team, meta)"""
     __slots__ = ()
     _fields = ('name', 'owner', 'description', 'level', 'team', 'meta', 'ustats')
 
@@ -99,14 +98,14 @@ class Character(tuple):
 
     @classmethod
     def _make(cls, iterable, new=tuple.__new__, len=len):
-        'Make a new Character object from a sequence or iterable'
+        """Make a new Character object from a sequence or iterable"""
         result = new(cls, iterable)
         if len(result) != 6:
             raise TypeError('Expected 6 arguments, got %d' % len(result))
         return result
 
     def _replace(_self, **kwds):
-        'Return a new Character object replacing specified fields with new values'
+        """Return a new Character object replacing specified fields with new values"""
         result = _self._make(map(kwds.pop, ('name', 'owner', 'description', 'level', 'team', 'meta'), _self))
         if kwds:
             raise ValueError('Got unexpected field names: %r' % list(kwds))
