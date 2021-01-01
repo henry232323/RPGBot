83b68e65d6d02ead9cebe2ad904ad953099b17bd
henry232323 <14969260+henry232323@users.noreply.github.com>
2021-01-01 16:46:59 -0700

    fixerooni

--- cogs/characters.py
+++ cogs/characters.py
@@ -96,7 +96,7 @@ class Characters(commands.Cog):
             if char.meta.get("image"):
                 embed.set_image(url=char.meta["image"])
             if char.meta.get("icon"):
-                embed.set_thumbnail(url=char.meta["image"])
+                embed.set_thumbnail(url=char.meta["icon"])
             embed.add_field(name=await _(ctx, "Name"), value=char.name)
             embed.add_field(name=await _(ctx, "Owner"), value=str(owner))
             if char.level is not None:
